0b4a259608def25705ddb767d2ce595cff0c3343
picasso09 <119201284+picasso09@users.noreply.github.com>
2023-11-26 23:58:23 +0700

    [TEST] drivers: gpu: Overclock GPU 900mhz
    misc tuning

--- drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
@@ -34,16 +34,16 @@
  * MT6765 segment_3 : GPU DVFS OPP table Setting
  **************************************************/
 #define SEG3_GPU_DVFS_FREQ0			(900000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ1			(730000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ2			(680000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ1			(800000)	/* KHz */
+#define SEG3_GPU_DVFS_FREQ2			(730000)	/* KHz */
 
-#define SEG3_GPU_DVFS_VOLT0			(90000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT1			(90000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT2			(90000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT0			(90500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT1			(90500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT2			(90500)		/* mV x 100 */
 
-#define SEG3_GPU_DVFS_VSRAM0			(97500)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VSRAM1			(97500)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VSRAM2			(97500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM0			(98500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM1			(98500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM2			(98500)		/* mV x 100 */
 
 /**************************************************
  * MT6765T segment_4 : GPU DVFS OPP table Setting
