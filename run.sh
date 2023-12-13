42c5d21bc610e87f25b8baf68d4785604841be2a
cezzane09 <153372582+cezzane09@users.noreply.github.com>
2023-12-13 15:17:01 +0000

    a

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export KBUILD_BUILD_HOST="picasso09"
 export KBUILD_BUILD_USER="picasso"
-export PATH="/workspaces/proton-clang/bin:$PATH"
+export PATH="$(pwd)/clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
