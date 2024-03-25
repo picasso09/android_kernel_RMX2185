fa8c979c742730d7fb7f082a5fa3ddf50435cfb2
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-03-25 09:22:40 +0000

    RMX218X: update KSU 0.9.2 [2]

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+# set environment variables
+git clone --depth=1 https://github.com/picasso09/proton-clang clang
+export LOCALVERSION="-DiscussionVerse-KSU"
 export KBUILD_BUILD_HOST="picasso09"
 export KBUILD_BUILD_USER="picasso"
 export PATH="$(pwd)/clang/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
-                          CROSS_COMPILE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+# build kernel
+make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
+make -j$(nproc --all) ARCH=arm64 O=out \
+                  CC=clang \
+                  CROSS_COMPILE=aarch64-linux-gnu- \
+                  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
