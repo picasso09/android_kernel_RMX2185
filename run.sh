3e3fba0c912f6e9e337f149422ea7b1129cd3d61
picasso09 <119201284+picasso09@users.noreply.github.com>
2024-05-09 13:36:36 +0000

    RMX2185: erofs: move from drivers/staging to fs Since 5.4, erofs has been moved into fs/ erofs set y by default Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 # set environment variables
-git clone --depth=1 https://github.com/picasso09/proton-clang clang
-export KBUILD_BUILD_HOST="picasso09"
-export KBUILD_BUILD_USER="picasso"
+#git clone --depth=1 https://github.com/picasso09/proton-clang clang
+export KBUILD_BUILD_HOST="DV-WORK"
+export KBUILD_BUILD_USER="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
 
 # build kernel
 make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
 make -j$(nproc --all) ARCH=arm64 O=out \
-                  CC=clang \
-                  CROSS_COMPILE=aarch64-linux-gnu- \
-                  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+                      HOSTCC="ccache clang" \
+                      HOSTCXX="ccache clang++" \
+                      CC="ccache clang" \
+                      LD=ld.lld \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      READELF=llvm-readelf \
+                      OBJSIZE=llvm-size \
+                      STRIP=llvm-strip \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
