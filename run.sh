b92d0630bea51ca47089c974ef657adc7ac55adb
picasso09 <sayazaki282901@gmail.com>
2024-06-27 11:48:40 +0700

    RMX2185: run: tracked clang-r522817
    Signed-off-by: picasso09
    Date:   Thu Jun 27 11:48:40 2024 +0700
    change-Id: b78da7d2e74a324cf315029b1de6fc3cbba8c87b

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 # set environment variables
 #git clone --depth=1 https://github.com/picasso09/proton-clang clang
+mkdir clang && cd clang2 && wget https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r522817.tar.gz && tar xf clang-r522817.tar.gz && rm clang-r522817.tar.gz && cd ..
 export KBUILD_BUILD_HOST="DV-WORK"
 export KBUILD_BUILD_USER="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
 
 # build kernel
-#make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-make -j1 ARCH=arm64 O=out \
+make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
+make -j$(nproc --all) ARCH=arm64 O=out \
                       HOSTCC="ccache clang" \
                       HOSTCXX="ccache clang++" \
                       CC="ccache clang" \
